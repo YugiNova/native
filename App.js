c23c39c25e8b0de727c350f6fb8a4034f45aa892
Yugi Nova <yuginovaniac@gmail.com>
2023-07-04 12:00:57 +0700

    test

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  const fetchData = () => {
+    axios.get("https://6401dc9d0a2a1afebef3c167.mockapi.io/users")
+    .then(res => {
+      result = res.data;
+      setData(result)
+    })
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <ScrollView style={styles.container}>
+      {
+        data.map(item => {
+          return <Text style={styles.text} key={item.id}>{item.name}</Text>
+        })
+      }
+      
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -14,7 +35,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  text: {
+    fontSize: 20
+  }
 });
